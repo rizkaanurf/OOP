df4832e3bfe47ff518e1b97f19025bcbe54740c5
rizkaanurf <rizkaanur16@gmail.com>
2019-03-28 08:55:37 +0700

    Upload project

---
+++ Bootcamp/OOP.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bootcamp
+{
+    //abstract class Hitung
+    //{
+    //    public abstract double Perhitungan();
+    //}
+
+    //class Segitiga : Hitung
+    //{
+    //    public int sisi1;
+    //    public int sisi2;
+    //    public int sisi3;
+
+    //    //constructor
+    //    public Segitiga(int a, int b, int c)
+    //    {
+    //        sisi1 = a;
+    //        sisi2 = b;
+    //        sisi3 = c;
+    //    }
+
+    //    //override
+    //    public override double Perhitungan()
+    //    {
+    //        return (sisi1 * sisi2 * sisi3);
+    //    }
+    //}
+
+    //class Lingkaran : Hitung
+    //{
+    //    public int jarijari;
+
+    //    //contructor
+    //    public Lingkaran(int r)
+    //    {
+    //        jarijari = r;
+    //    }
+
+    //    //override
+    //    public override double Perhitungan()
+    //    {
+    //        return (2*(22/7)*jarijari);
+    //    }
+    //}
+
+    //class PersegiPanjang : Hitung
+    //{
+    //    public int panjang;
+    //    public int lebar;
+
+    //    //constructor
+    //    public PersegiPanjang(int p, int l)
+    //    {
+    //        panjang = p;
+    //        lebar = l;
+    //    }
+
+    //    public override double Perhitungan()
+    //    {
+    //        return (2 * (panjang + lebar));
+    //    }
+    //}
+
+    //class Trapesium : Hitung
+    //{
+    //    public int A;
+    //    public int B;
+    //    public int C;
+    //    public int D;
+
+    //    //constructor
+    //    public Trapesium(int a, int b, int c, int d)
+    //    {
+    //        A = a;
+    //        B = b;
+    //        C = c;
+    //        D = d;
+    //    }
+
+    //    //override
+    //    public override double Perhitungan()
+    //    {
+    //        return ((A*B)+(B*C)+(C*D)+(D*A));
+    //    }
+    //}
+
+    //class OOP
+    //{
+    //    public static void Main(String[] args)
+    //    {
+    //        int n;
+    //        Console.WriteLine("-------------------------------");
+    //        Console.WriteLine("PERHITUNGAN KELILING");
+    //        Console.WriteLine("-------------------------------");
+    //        Console.WriteLine("1. Segitiga");
+    //        Console.WriteLine("2. Lingkaran");
+    //        Console.WriteLine("3. Persegi Panjang");
+    //        Console.WriteLine("4. Trapesium");
+    //        Console.WriteLine("-------------------------------");
+    //        Console.Write("Masukkan pilihan mu : ");
+    //        n = Convert.ToInt16(Console.ReadLine());
+    //        Console.WriteLine("-------------------------------");
+    //        if (n == 1)
+    //        {
+    //            Console.WriteLine();
+    //            Console.WriteLine("Keliling Segitiga");
+    //            Console.Write("Masukkan Sisi 1 : ");
+    //            int s1 = Convert.ToInt16(Console.ReadLine());
+    //            Console.Write("Masukkan Sisi 2 : ");
+    //            int s2 = Convert.ToInt16(Console.ReadLine());
+    //            Console.Write("Masukkan Sisi 3 : ");
+    //            int s3 = Convert.ToInt16(Console.ReadLine());
+    //            Hitung s = new Segitiga(s1, s2, s3);
+    //            double kelSegitiga = s.Perhitungan();
+    //            Console.Write("Keliling = " + kelSegitiga);
+    //        } else if (n == 2)
+    //        {
+    //            Console.WriteLine();
+    //            Console.WriteLine("Keliling Lingkaran");
+    //            Console.Write("Masukkan Jari-jari : ");
+    //            int r = Convert.ToInt16(Console.ReadLine());
+    //            Hitung l = new Lingkaran(r);
+    //            double kelLingkaran = l.Perhitungan();
+    //            Console.Write("Keliling = " + kelLingkaran);
+    //        }else if (n == 3)
+    //        {
+    //            Console.WriteLine();
+    //            Console.WriteLine("Keliling Persegi Panjang");
+    //            Console.Write("Masukkan Panjang : ");
+    //            int p = Convert.ToInt16(Console.ReadLine());
+    //            Console.Write("Masukkan Lebar : ");
+    //            int l = Convert.ToInt16(Console.ReadLine());
+    //            Hitung pp = new PersegiPanjang(p,l);
+    //            double kelPersegiPanjang = pp.Perhitungan();
+    //            Console.Write("Keliling = " + kelPersegiPanjang);
+    //        }else if ( n == 4)
+    //        {
+    //            Console.WriteLine();
+    //            Console.WriteLine("Keliling Trapesium");
+    //            Console.Write("Masukkan nilai A : ");
+    //            int a = Convert.ToInt16(Console.ReadLine());
+    //            Console.Write("Masukkan nilai B : ");
+    //            int b = Convert.ToInt16(Console.ReadLine());
+    //            Console.Write("Masukkan nilai C : ");
+    //            int c = Convert.ToInt16(Console.ReadLine());
+    //            Console.Write("Masukkan nilai D : ");
+    //            int d = Convert.ToInt16(Console.ReadLine());
+    //            Hitung t = new Trapesium(a,b,c,d);
+    //            double kelTrapesium = t.Perhitungan();
+    //            Console.Write("Keliling = " + kelTrapesium);
+    //        }
+    //        else
+    //        {
+    //            Console.Write("Masukkan anda tidak tepat");
+    //        }
+    //        Console.Read();
+    //    }
+    //}
+}
---
+++ Bootcamp/Program.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bootcamp
+{
+    //class Program
+    //{
+    //    static Program calling = new Program();
+    //    static void Main(string[] args)
+    //    {
+    //        calling.view();
+    //    }
+
+    //    void view()
+    //    {
+    //        //deklarasi variabel
+    //        int a, b;
+
+    //        //inputan dari user
+    //        Console.Write("Insert a number : ");
+    //        a = Convert.ToInt16(Console.ReadLine());
+    //        Console.Write("Insert b number : ");
+    //        b = Convert.ToInt16(Console.ReadLine());
+
+    //        //pemanggilan method perkalian
+    //        Console.Write("Hasil : " + calling.Perkalian(a, b));
+    //        Console.Read();
+    //    }
+
+    //    public int Perkalian(int a, int b)
+    //    {
+    //        int sum = 0;
+    //        if (b < 0)
+    //        {
+    //            for (int i = 0; i > b; i--)
+    //            {
+    //                sum = sum + (a * (-1));
+    //            }
+    //        }
+    //        else
+    //        {
+    //            for (int i = 0; i < b; i++)
+    //            {
+    //                sum = sum + a;
+    //            }
+    //        }
+    //        return sum;
+    //    }
+    //}
+
+    //public class Parent
+    //{
+    //    public int a;
+    //    public int b;
+    //}
+
+    //public class Child : Parent, Isupp
+    //{
+    //    public int getDataFromParent()
+    //    {
+    //        return (a * b);
+    //    }
+    //}
+
+    //public interface Isupp
+    //{
+
+    //}
+    
+    //public class Mahasiswa
+    //{
+    //    private string name;
+    //    private string nim;
+
+    //    public void View()
+    //    {
+    //        //this.name = "Rizka";
+    //    }
+    //}
+    
+
+    //public class Jurusan
+    //{
+    //    static void Main(string[] args)
+    //    {
+    //    Mahasiswa mhs = new Mahasiswa();
+    //        mhs.View();
+    //     }
+    //}
+
+    abstract class Process
+    {
+        public abstract decimal Processing();
+    }
+
+    class Rectangle : Process
+    {
+        public int length;
+        public int width;
+
+        //contructor
+        public Rectangle(int a, int b)
+        {
+            length = a;
+            width = b;
+        }
+
+        //overide
+        public override decimal Processing()
+        {
+            return (length * width);
+        }
+    }
+
+    class Circle : Process
+    {
+        public int jarijari;
+
+        //contructor
+        public Circle(int a)
+        {
+            jarijari = a;
+        }
+
+        public override decimal Processing()
+        {
+            return (2*(22/7)*jarijari);
+        }
+    }
+
+    class RectangleProcess
+    {
+        //public static void Main(String[] args)
+        //{
+        //    Process r = new Rectangle(10, 7);
+        //    decimal l = r.Processing();
+        //    Console.Write("Luas : " + l);
+        //    Console.WriteLine();
+
+        //    Process c = new Circle(4);
+        //    decimal k = c.Processing();
+        //    Console.Write("Keliling : " + k);
+        //    Console.Read();
+        //}
+    }
+}
